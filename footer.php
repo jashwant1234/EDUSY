37ebdfa9b14ac369a21f8c69dadc80571f907bf3
jashwant pradhan <jpradhan727@gmail.com>
2020-09-01 08:41:22 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/1.png" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  <!-- Start your project here-->  
+  <!-- Footer -->
+<footer class="page-footer font-small blue-gradient pt-4">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <!-- Footer links -->
+  <div class="row text-center text-md-left mt-3 pb-3">
+
+    <!-- Grid column -->
+    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
+      <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+        consectetur
+        adipisicing elit.</p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+      <p>
+        <a href="#!">MDBootstrap</a>
+      </p>
+      <p>
+        <a href="#!">MDWordPress</a>
+      </p>
+      <p>
+        <a href="#!">BrandFlow</a>
+      </p>
+      <p>
+        <a href="#!">Bootstrap Angular</a>
+      </p>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+      <p>
+        <a href="#!">Your Account</a>
+      </p>
+      <p>
+        <a href="#!">Become an Affiliate</a>
+      </p>
+      <p>
+        <a href="#!">Shipping Rates</a>
+      </p>
+      <p>
+        <a href="#!">Help</a>
+      </p>
+    </div>
+
+    <!-- Grid column -->
+    <hr class="w-100 clearfix d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+      <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+      <p>
+        <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+      <p>
+        <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+      <p>
+        <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+      <p>
+        <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Footer links -->
+
+  <hr>
+
+  <!-- Grid row --> 
+ <div class="row d-flex align-items-center">
+
+    <!-- Grid column -->
+    <div class="col-md-7 col-lg-8">
+
+      <!--Copyright-->
+      <p class="text-center text-md-left">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/">
+          <strong> MDBootstrap.com</strong>
+        </a>
+      </p>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-5 col-lg-4 ml-lg-0">
+
+      <!-- Social buttons -->
+      <div class="mb-5 flex-center">
+
+<!-- Facebook -->
+<a class="fb-ic">
+<i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
+</a>
+<!-- Twitter -->
+<a class="tw-ic">
+<i class="fab fa-twitter fa-lg white-text mr-4"> </i>
+</a>
+<!-- Google +-->
+<a class="gplus-ic">
+<i class="fab fa-google-plus-g fa-lg white-text mr-4"> </i>
+</a>
+<!--Linkedin -->
+<a class="li-ic">
+<i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
+</a>
+<!--Instagram-->
+<a class="ins-ic">
+<i class="fab fa-instagram fa-lg white-text mr-4"> </i>
+</a>
+<!--Pinterest-->
+<a class="pin-ic">
+<i class="fab fa-pinterest fa-lg white-text"> </i>
+</a>
+
+</div>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
+  <!-- End your project here-->
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+</body>
+
+</html>
